fab8553e74a9597d47d9f6bbfbb8ff4a48d3ba87
Mishael Matende <mishaelmatende@gmail.com>
2024-10-28 23:52:01 +0300

    Updated components

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,27 @@
 import React from "react";
 
 export default function Hero() {
-  return <div>Hero</div>;
+  return (
+    <div className="min-h-screen flex flex-col gap-10 items-center justify-center text-center max-w-[800px] w-full mx-auto p-4">
+      <div className="flex flex-col gap-4">
+        <p>IT'S HIGH TIME TO GET SOME</p>
+        <h1 className="font-semibold text-4xl sm:text-5xl md:text-6xl lg:text-7xl">
+          BOLDER
+          <span className="text-blue-400">MOTIV</span>
+        </h1>
+      </div>
+
+      <p className="text-sm md:text-base font-light">
+        I hereby acknowledge that I am voluntarily signing up for this workout
+        <span className="text-blue-400 font-medium">(temple of sweat)</span>,
+        knowing full well that my muscles may temporarily resent me - but{" "}
+        <span className="text-blue-400 font-medium">
+          my future me will thank me!
+        </span>
+      </p>
+      <button className="px-8 py-4 rounded-md border-[2px] bg-slate-950 border-blue-400 border-solid blueshadow duration-200">
+        <p>Accept & Begin</p>
+      </button>
+    </div>
+  );
 }
